ec11e436ba75900b048ad73a4ed33a17759b5eb6
pmoreaualfa <philippe.moreau@alfa3a.org>
2022-12-09 18:32:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nuxt3-appCurrency
+ Vue Mastery Nuxt 3 Essentials
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>
+    <NuxtLink to="/"> Home </NuxtLink>
+  </nav>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
